c5fcf681c1825a4655e436e4900f5b3852ab6617
jason6356 <xiaodemon6536@gmail.com>
2023-07-29 01:22:52 +0800

    Refactored the Component

--- SainoPal-Mobile/src/screens/Scan/Scan.tsx
+++ SainoPal-Mobile/src/screens/Scan/Scan.tsx
@@ -1,28 +1,30 @@
-import React, { useState, useEffect } from "react"
-import { Text, View, StyleSheet, Button, Alert } from "react-native"
-import { BarCodeScanner } from "expo-barcode-scanner"
-import AgentService from "../../services/AgentService"
-import { Buffer } from "buffer"
-import axios from "axios"
-export default function App() {
-  const [hasPermission, setHasPermission] = useState(null)
-  const [scanned, setScanned] = useState(false)
-  const [agent, setAgent] = useState<AgentService | null>(null)
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet, Button, Alert } from "react-native";
+import { BarCodeScanner, BarCodeScannerResult } from "expo-barcode-scanner";
+import AgentService from "../../services/AgentService";
+import { Buffer } from "buffer";
+import axios from "axios";
+
+export default function Scan() {
+  const [hasPermission, setHasPermission] = useState<boolean | null>(null);
+  const [scanned, setScanned] = useState(false);
+  const [agent, setAgent] = useState<AgentService | null>(null);
 
   useEffect(() => {
     const getBarCodeScannerPermissions = async () => {
-      const { status } = await BarCodeScanner.requestPermissionsAsync()
-      setHasPermission(status === "granted")
-      const initAgent: AgentService = new AgentService(axios)
-      setAgent(initAgent)
-    }
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      if (status === "granted") {
+        setHasPermission(true);
+        const initAgent: AgentService = new AgentService(axios);
+        setAgent(initAgent);
+      }
+    };
 
-    getBarCodeScannerPermissions()
-  }, [])
+    getBarCodeScannerPermissions();
+  }, []);
 
-  const handleBarCodeScanned = ({ type, data }) => {
+  const handleBarCodeScanned = ({ type, data } : BarCodeScannerResult) => {
     // setScanned(true)
-
     Alert.alert(
       "Scanned Data",
       `Bar code with type ${type} and data ${data} has been scanned!`,
@@ -31,40 +33,40 @@ export default function App() {
           text: "Cancel",
           style: "cancel",
           onPress: () => {
-            setScanned(false)
+            setScanned(false);
           },
         },
         {
           text: "OK",
           onPress: () => {
-            const url = new URL(data)
-            const invitationParam = url.searchParams.get("c_i")
+            const url = new URL(data);
+            const invitationParam = url.searchParams.get("c_i");
 
             if (!invitationParam) {
-              console.error("Invalid Invitation Url")
-              return
+              console.error("Invalid Invitation Url");
+              return;
             }
-            const decoded = Buffer.from(invitationParam, "base64").toString()
-            console.log(decoded)
-            const payload = JSON.parse(decoded)
-            console.log("Payload: ", payload)
+            const decoded = Buffer.from(invitationParam, "base64").toString();
+            console.log(decoded);
+            const payload = JSON.parse(decoded);
+            console.log("Payload: ", payload);
 
             agent
               ?.receiveInvitation(payload)
               .then((e) => console.log(e))
-              .catch((e) => console.error(e))
+              .catch((e) => console.error(e));
           },
         },
       ],
       { cancelable: false }
-    )
-  }
+    );
+  };
 
   if (hasPermission === null) {
-    return <Text>Requesting for camera permission</Text>
+    return <Text>Requesting for camera permission</Text>;
   }
   if (hasPermission === false) {
-    return <Text>No access to camera</Text>
+    return <Text>No access to camera</Text>;
   }
 
   return (
@@ -79,7 +81,7 @@ export default function App() {
         <Button title={"Tap to Scan Again"} onPress={() => setScanned(false)} />
       )}
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -97,4 +99,4 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-})
+});
